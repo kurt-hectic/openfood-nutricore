6586610dca3d7a6c6916943f628e5f6250b0f5c5
Kirtan Padh <kirtan.701@gmail.com>
2018-03-07 20:31:47 +0100

    update repository

---
+++ food_db.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+def build_database():
+	"""List of fruits and vegetables"""
+	
+	a = ['apple', 'avocado', 'banana', 'blackberry', 'cherry', 'chestnut', 'clementine', 'coconut', 'date', 'fig', 'guava', 'grape', 'lemon', 'lime', 'mandarin', 'mango', 'melon', 'orange', 'papaya', 'peach', 'pear', 'pineapple', 'plum', 'walnut', 'strawberry', 'watermelon', 'pomme', 'avocat', 'banane', 'cerise', 'clementine', 'noix de coco', 'figue', 'raisin', 'kiwi', 'citron', 'lime', 'mandarine', 'mangue', 'melon', 'orange', 'papaye', 'poire', 'ananas', 'prune', 'framboise', 'noix', 'pamplemousse', 'noisette', 'asparagus', 'bean', 'brocolli', 'cabbage', 'carrot', 'celery', 'chick pea', 'courgette', 'eggplant', 'endive', 'garlic', 'leek', 'lettuce', 'lentils', 'maize', 'mushroom', 'onion', 'pea', 'pepper', 'pumpkin', 'potato', 'spinach', 'tomato', 'soya bean', 'asperge', 'haricot', 'poivron', 'brocoli', 'chou', 'carotte', 'manioc', 'chou fleur', 'celeri', 'chechi', 'courgette', 'aubergine', 'ail', 'lentille', 'mais', 'champignon', 'ortie', 'petit pois', 'piment', 'citrouille', 'courge', 'tomate', 'soja', 'epinard', 'apfel', 'birne', 'kirche', 'melone', 'traube', 'grapefruit', 'nuss', 'erdnuss', 'walnuss', 'kokosnuss', 'kortoffel', 'gurke', 'radieschen', 'erbse', 'linse', 'brokkoli', 'blumenkohl', 'bohne', 'zucchini', 'zwiebel', 'knoblauch', 'chilli', 'petersilie', 'meerrettich', 'sellerie']
+	
+	return a
+ 
